5985babbc757e41976523786ec590e25db65b1ea
motoi-tsushima <motoi.tsushima@gmail.com>
2021-01-23 17:27:18 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample_gRPC_ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Grpc.Net.Client;
+
+namespace Sample_gRPC_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = client.SayHello(new HelloRequest { Name = "Motoi.Tsushima" });
+            //var response = client.SayHelloAsync(new HelloRequest { Name = "World" });
+
+            Console.WriteLine("Greeting: " + response.Message);
+        }
+    }
+}
