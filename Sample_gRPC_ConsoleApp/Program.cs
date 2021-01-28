4e0ab996ec600f7cd4917b7d245d75f4e44681b8
motoi-tsushima <motoi.tsushima@gmail.com>
2021-01-28 17:58:14 +0900

    tag 1.1.0.0 MyFunction と Calc を追加した。

--- Sample_gRPC_ConsoleApp/Program.cs
+++ Sample_gRPC_ConsoleApp/Program.cs
@@ -14,6 +14,36 @@ namespace Sample_gRPC_ConsoleApp
             //var response = client.SayHelloAsync(new HelloRequest { Name = "World" });
 
             Console.WriteLine("Greeting: " + response.Message);
+
+            Console.WriteLine("");
+
+            // my function
+            string requestText = "MyRequest";
+            Int32 intValue = 23;
+
+            MyRequest myRequest = new MyRequest();
+            myRequest.Parameter1 = requestText;
+            myRequest.ParameterIntValue = intValue;
+
+            var reply = client.MyFunction(myRequest);
+
+            Console.WriteLine("MyFunction: " + reply.Message);
+
+            Console.WriteLine("");
+
+            // calc function
+            Int32 intValue1 = 50;
+            Int32 intValue2 = 5;
+
+            var replyCalc = client.Calc(
+                new CalcParameter { Parameter1 = intValue1, Parameter2 = intValue2 }
+                );
+
+            Console.WriteLine("Calc 結果");
+            Console.WriteLine("　加算=" + replyCalc.Addition);
+            Console.WriteLine("　減算=" + replyCalc.Subtraction);
+            Console.WriteLine("　掛算=" + replyCalc.Multiplication);
+            Console.WriteLine("　割算=" + replyCalc.Division);
         }
     }
 }
